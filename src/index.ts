d5021872ff3617a389d17a2f21a8847bc1510795
Jan Klimo <jan.klimo@gmail.com>
2022-10-10 12:26:16 +0700

    Fetching logic
    Make sure to refetch if object size is zero

--- src/index.ts
+++ src/index.ts
@@ -1,30 +1,69 @@
-/**
- * Welcome to Cloudflare Workers! This is your first worker.
- *
- * - Run `wrangler dev src/index.ts` in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your worker in action
- * - Run `wrangler publish src/index.ts --name my-worker` to publish your worker
- *
- * Learn more at https://developers.cloudflare.com/workers/
- */
-
-export interface Env {
-  // Example binding to KV. Learn more at https://developers.cloudflare.com/workers/runtime-apis/kv/
-  // MY_KV_NAMESPACE: KVNamespace;
-  //
-  // Example binding to Durable Object. Learn more at https://developers.cloudflare.com/workers/runtime-apis/durable-objects/
-  // MY_DURABLE_OBJECT: DurableObjectNamespace;
-  //
-  // Example binding to R2. Learn more at https://developers.cloudflare.com/workers/runtime-apis/r2/
-  // MY_BUCKET: R2Bucket;
+interface Env {
+  R2: R2Bucket;
+  AWS_HOSTNAME: string;
 }
+
+const objectNotFound = (objectName: string): Response =>
+  new Response(`Object ${objectName} not found`, {
+    status: 404,
+  });
 
 export default {
   async fetch(
     request: Request,
     env: Env,
-    ctx: ExecutionContext,
+    ctx: EventContext<any, any, any>,
   ): Promise<Response> {
-    return new Response("Hello World!")
+    const url = new URL(request.url);
+    const objectName = url.pathname.slice(1);
+
+    if (objectName === "") {
+      return new Response(`Bad Request`, {
+        status: 400,
+      });
+    }
+
+    if (request.method !== "GET") {
+      return new Response(`Method Not Allowed`, {
+        status: 405,
+      });
+    }
+
+    const obj = await env.R2.get(objectName);
+
+    /**
+     * An object can be fetched successfully but the write somehow fail
+     * ending up with an object of size 0. If that happens, refetch and
+     * attempt to save it until it succeeds.
+     */
+    if (obj === null || obj.size === 0) {
+      url.protocol = "https:";
+      url.hostname = env.AWS_HOSTNAME;
+
+      console.log("Fetching:", url.toString());
+
+      const s3Object = await fetch(url.toString());
+
+      if (s3Object.status === 403 || s3Object.status === 404) {
+        return objectNotFound(objectName);
+      }
+
+      const s3Body = s3Object.body!.tee();
+      ctx.waitUntil(
+        env.R2.put(objectName, s3Body[0], {
+          httpMetadata: s3Object.headers,
+        }),
+      );
+
+      return new Response(s3Body[1], s3Object);
+    }
+
+    const headers = new Headers();
+    obj.writeHttpMetadata(headers);
+    headers.set("etag", obj.httpEtag);
+
+    return new Response(obj.body, {
+      headers,
+    });
   },
-}
+};
